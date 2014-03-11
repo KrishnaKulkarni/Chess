9b73c33b369304818e548bb813eb32016e0feb4a
Krishna Kulkarni & Patrick Gardocki <krishna-patrick@appacademy.io>
2014-03-11 15:27:16 -0400

    Creates moves for all pieces, except pawns; accounts for blocked spaces

--- chess.rb
+++ chess.rb
@@ -20,7 +20,7 @@ module Chess
 
     #later on add the feature that you can create a Board mid-game
     # => a filled grid(8x8)
-    def Board.default_setup
+    def Board.default_setup(board)
       grid = Array.new(8) { Array.new(8) }
 
       #could make more slick with better choice of iterators
@@ -28,7 +28,7 @@ module Chess
       [:white, :black].each do |color|
         8.times do |col|
           row = (color == :white) ? 1 : 6
-          p = Pawn.new(color, [row, col], grid)
+          p = Pawn.new(color, [row, col], board)
           pawn_row << p
         end
       end
@@ -42,14 +42,14 @@ module Chess
           row = (color == :white) ? 0 : 7
 
           case col
-          when 0 then p = Rook.new(color, [row, col], grid)
-          when 1 then p = Knight.new(color, [row, col], grid)
-          when 2 then p = Bishop.new(color, [row, col], grid)
-          when 3 then p = King.new(color, [row, col], grid)
-          when 4 then p = Queen.new(color, [row, col], grid)
-          when 5 then p = Bishop.new(color, [row, col], grid)
-          when 6 then p = Knight.new(color, [row, col], grid)
-          when 7 then p = Rook.new(color, [row, col], grid)
+          when 0 then p = Rook.new(color, [row, col], board)
+          when 1 then p = Knight.new(color, [row, col], board)
+          when 2 then p = Bishop.new(color, [row, col], board)
+          when 3 then p = King.new(color, [row, col], board)
+          when 4 then p = Queen.new(color, [row, col], board)
+          when 5 then p = Bishop.new(color, [row, col], board)
+          when 6 then p = Knight.new(color, [row, col], board)
+          when 7 then p = Rook.new(color, [row, col], board)
           end
 
           piece_row << p
@@ -62,8 +62,37 @@ module Chess
     end
 
 
-    def initialize(grid = Board.default_setup)
+    def initialize(grid = Board.default_setup(self))
       @grid = grid
+    end
+
+    def in_check?(color)
+      # find the right-colored king's position
+      # -> nested iteration through whole grid, until grid[position] == KING
+      # ---> if there is no king, raise error
+      #
+      # find if any other piece can move there
+      # Algorithm1: iterate through every other-colored piece
+      # -> if that piece's moves_array includes the King's position => in check
+      # Algorithm2: starting at the king's square, go through all the ways a piece can move (e..g DIAGONALS, KNIGHTS_DIRs, etc.)
+      # if, by moving in that direction, the KING could reach the square of an other-colored piece of the right type => in check (e.g. if by moving diagonally, the white King could reach a black bishop or a black Queen, then the King is in check)
+
+    end
+
+    def move(start_pos, end_pos)
+      # (1) get the piece from the start_pos
+      # --> raise/rescue error if there is no piece in start_pos
+      # (2) move the piece to the proper spot
+      # --> raise/rescue error if the end_pos is not included in the piece's moves
+      #   (2a) assign the board[end_pos] to the new piece
+      #   (2b) assign the board[start_pos] to nil
+      #   (2c) reassign the piece's position to end_pos
+
+    end
+
+    def [](position)
+      x,y = position
+      self.grid[x][y]
     end
 
 
@@ -90,6 +119,10 @@ module Chess
     private
     def on_board?(move_pos)
       move_pos.first.between?(0,7) && move_pos.last.between?(0,7)
+    end
+
+    def not_blocked?(pos)
+      self.board[pos].nil? || (self.board[pos].color != self.color)
     end
 
 
@@ -126,13 +159,34 @@ module Chess
       #    new_arr << [3, -3]
       # adders = [ [1,1], [2,2], [3,3], [1,-1], [2, -2], ...   ]
 
-     directions.map do |(dx, dy)|
-       multipliers.map do |mult|
-          x, y = self.position
-         [x + dx * mult, y + dy * mult]
+      # move_arr should not stop adding the moves in a direction that becomes blocked by a different piece:
+      # break from the multipliers loop if the direction is blocked
+      # --  if the current position is occupied by an other-colored piece
+      # --- or if the subsequent position is occupied by a same-colored piece
+      poss_moves = []
+      poss_moves.tap do
+        directions.each do |(dx, dy)|
+          multipliers.each do |mult|
+            x, y = self.position
 
-       end
-     end.flatten(1).select { |coord| on_board?(coord) }
+            cand_pos = [x + dx * mult, y + dy * mult]
+            break if !(not_blocked?(cand_pos))
+
+            poss_moves << cand_pos
+            break if board[cand_pos] && (board[cand_pos].color != self.color)
+          end
+        end
+      end.select { |coord| on_board?(coord) }
+
+
+     #
+     # directions.map do |(dx, dy)|
+     #   multipliers.map do |mult|
+     #      x, y = self.position
+     #     [x + dx * mult, y + dy * mult]
+     #
+     #   end
+     # end.flatten(1).select { |coord| on_board?(coord) }
 
 
     end
@@ -151,13 +205,21 @@ module Chess
     # k1.moves
     # ---> k1.piece_type => :king
     # our_hash[:king] => an array of the directions a king can move in
+    # Don't add the move to move_array if the space is occupied by a same-colored piece
+
+    #FUTURE EDIT: we could make the boolean condition for select more slick
     def moves
       directions = MOVE_DIRECTIONS[self.piece_type]
       directions.map do |(dx, dy)|
         x, y = self.position
         [x + dx, y + dy]
-      end.select { |coord| on_board?(coord) }
+      end.select { |coord| on_board?(coord) && not_blocked?(coord) }
 
+      # cond = board[[coord]].nil? || board[[coord]].color != self.color
+      # opp_cond = !(board[coord].color == self.color)
+      # coord = [2,4]
+      # [[2,3], nil, [3,4]]
+      #(self.board[coord].color != self.color)
     end
 
 
